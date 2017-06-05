ad42c8a2bdbbcf539ef8e47cc47aefa2347c6c08
MD. Jahidul Islam <jsec516@gmail.com>
2017-06-06 02:34:43 +0600

    error module added

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ console.time('RugalC boot');
 var debug = require('debug')('rugalC:boot:index'),
     rugalC, rugalCIns, express, logging, errors, utils, parentApp;
 
-rugalC      = require('./core');
+rugalC      = require('./lib');
 express     = require('express');
 parentApp   = express();
 //@TODO: separate them on repo
-logging = require('./core/server/logging');
-errors = require('./core/server/errors');
-utils = require('./core/server/utils');
+logging = require('./lib/server/logging');
+errors = require('./lib/server/errors');
+utils = require('./lib/server/utils');
 
 debug('Initializing Rugal Core');
 rugalC().then(function startServer(rugalServer) {
@@ -21,9 +21,9 @@ rugalC().then(function startServer(rugalServer) {
         debug('Server get started');
     })
 }).catch(function handleInsError(err) {
-    // if (!errors.utils.isIgnitionError(err)) {
-    //     err = new errors.RugalError({err: err});
-    // }
+    if (!errors.utils.isIgnitionError(err)) {
+        err = new errors.RugalCError({err: err});
+    }
 
     if (process.send) {
         process.send({started: false, error: err.message});
